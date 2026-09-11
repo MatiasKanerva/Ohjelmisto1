a39a6e39cf22737691d5363db027d29f0fa19b4a
Matias Kanerva <matias.kanerva2@live.com>
2026-09-11 11:28:53 +0300

    update mo8

--- Mod8/Mod8-1.py
+++ Mod8/Mod8-1.py
@@ -1,4 +1,7 @@
 
-Kuukaudet = ("Tammi", "Helmi", "Maalis" "Huhti", "Touko", "Kesä", "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu")
-
+Kuukaudet = ("Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu")
+Vuodenajat = ("Talvi", "Kevät", "Kesä", "Syksy", "Talvi")
 Luku = int(input("Kerro kuukauden numero: "))
+Vuosi = Luku / 3
+print(Kuukaudet[Luku - 1])
+print(Vuodenajat[int(Vuosi)])
